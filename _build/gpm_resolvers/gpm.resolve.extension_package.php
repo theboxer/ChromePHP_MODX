336e7b29c6c36fefb6d5748b09f0a4bbaa416164
Jan Peca <pecajan@gmail.com>
2015-01-28 13:33:42 +0100

    ChromePHP 1.1.0

--- _build/gpm_resolvers/gpm.resolve.extension_package.php
+++ _build/gpm_resolvers/gpm.resolve.extension_package.php
@@ -1,27 +1,39 @@
 <?php
+/**
+ * Resolve creating db tables
+ *
+ * THIS RESOLVER IS AUTOMATICALY GENERATED, NO CHANGES WILL APPLY
+ *
+ * @package chromephp
+ * @subpackage build
+ */
 
 if ($object->xpdo) {
+    /** @var modX $modx */
+    $modx =& $object->xpdo;
+
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
-            /** @var modX $modx */
-            $modx =& $object->xpdo;
             $modelPath = $modx->getOption('chromephp.core_path');
+
             if (empty($modelPath)) {
                 $modelPath = '[[++core_path]]components/chromephp/model/';
             }
+
             if ($modx instanceof modX) {
-                $modx->addExtensionPackage('chromephp',$modelPath, array(
+                $modx->addExtensionPackage('chromephp', $modelPath, array(
                     'serviceName' => 'chromephp',
                     'serviceClass' => 'ChromePHP_MODX'
                 ));
             }
+
             break;
         case xPDOTransport::ACTION_UNINSTALL:
-            $modx =& $object->xpdo;
             if ($modx instanceof modX) {
                 $modx->removeExtensionPackage('chromephp');
             }
+
             break;
     }
 }
